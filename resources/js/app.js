a34b7716b644841ce4c2bb29f16c36561d057efb
Felipe Alves <felipealves1550@gmail.com>
2022-01-19 01:57:08 -0300

    Instalando Vue e Inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,31 +1,11 @@
-require('./bootstrap');
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
 
-// ----------------------    CONSTANTES    ----------------------
-
-// Elementos
-const searchInput = document.getElementById('search-input'),
-    searchResults = document.getElementById('search-results')
-
-// Funções
-const searchVisible = (visible) => {
-    const inputActive = ['bg-white', 'text-black', 'placeholder-slate-400'],
-        inputInactive = ['bg-white/50', 'text-white', 'placeholder-white'],
-        resultsActive = ['hidden']
-
-    if (visible) {
-        searchInput.classList.remove(...inputInactive)
-        searchInput.classList.add(...inputActive)
-        searchResults.classList.remove(...resultsActive)
-    } else {
-        searchInput.classList.add(...inputInactive)
-        searchInput.classList.remove(...inputActive)
-        searchResults.classList.add(...resultsActive)
-    }
-}
-
-
-// ----------------------    EVENTOS    ----------------------
-
-// Pesquisa
-searchInput.addEventListener('focus', () => searchVisible(true))
-searchInput.addEventListener('blur', () => searchVisible(false))
+createInertiaApp({
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+})
