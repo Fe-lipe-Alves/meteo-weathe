e2a548fde4a08ba978ede30332a46a602fd55389
Felipe Alves <felipealves1550@gmail.com>
2022-01-30 01:09:31 -0300

    Adicionando estilos personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,22 @@ module.exports = {
             fontsize: {
                 'xs': '.75rem',
                 'sm': '.875rem',
+            },
+            colors: {
+                'grayish-white': {
+                    100: '#F8F8F8',
+                    200: '#F2F2F2',
+                },
+                'minimum': '#404040',
             }
         },
         fontFamily: {
             sans: [
                 'Raleway',
+                ...defaultTheme.fontFamily['sans']
+            ],
+            roboto: [
+                'Roboto',
                 ...defaultTheme.fontFamily['sans']
             ]
         },
