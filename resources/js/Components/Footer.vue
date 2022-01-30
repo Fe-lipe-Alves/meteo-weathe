6269c861df82c890a44c4f9e352f3dfefd54195a
Felipe Alves <felipealves1550@gmail.com>
2022-01-30 01:08:19 -0300

    Criando o componente Footer

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <footer class="w-full mt-20 bg-[#343434]/75 flex flex-wrap pb-2 lg:pb-0">
+        <div class="w-full lg:w-6/12 flex justify-center">
+            <a href="" class="m-3"><img src="/images/icons/social/twitter.png" alt="Compartilhar no Twitter"></a>
+            <a href="" class="m-3"><img src="/images/icons/social/facebook.png" alt="Compartilhar no Facebook"></a>
+            <a href="" class="m-3"><img src="/images/icons/social/instagram.png" alt="Compartilhar no Instagram"></a>
+            <a href="" class="m-3"><img src="/images/icons/social/linkedin.png" alt="Compartilhar no LinkedIn"></a>
+        </div>
+
+
+        <div class="w-full lg:w-6/12 flex justify-center items-center">
+            <p class="text-center text-gray-100 text-xs">Meteo Weather - 2022 | Desenvolvido por Felipe Alves</p>
+        </div>
+    </footer>
+
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
