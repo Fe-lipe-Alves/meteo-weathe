43d498fadfcbe1d88d567559d6d152797c68bf87
Felipe Alves <felipealves1550@gmail.com>
2022-03-05 02:19:42 -0300

    Chamando as consultas de API e tratando o resultado

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\IpInfo;
+use App\Services\Tomorrow;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
+use PHPUnit\Exception;
 
 class HomeController extends Controller
 {
-    public function index()
+    /**
+     * Trata as consultas de API e rendezira a tela principal
+     *
+     * @param Request $request
+     * @return Response
+     * @throws \Exception
+     */
+    public function index(Request $request): Response
     {
-        return Inertia::render('Home', [
-            'name' => 'Felipe'
-        ]);
+        try {
+            //        $ipClient = $request->ip();
+            $ipClient = '186.193.118.203';
+            $consultIp = IpInfo::consultIp($ipClient);
+
+            $tomorrow = new Tomorrow($consultIp['loc'], $consultIp['timezone']);
+
+            $today = $tomorrow->today();
+            $today['location'] = $consultIp['city'];
+
+            $nextDays = $tomorrow->nextDays();
+
+            return Inertia::render('Home', [
+                'dataToday'    => $today,
+                'dataNextDays' => $nextDays,
+            ]);
+        } catch (Exception) {
+            return Inertia::render('Error');
+        }
     }
 }
