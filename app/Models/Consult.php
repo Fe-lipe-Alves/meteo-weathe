00a2a87f8d7697d70116712305cd41bfcc899a65
Felipe Alves <felipealves1550@gmail.com>
2022-03-05 02:16:19 -0300

    Criando modelo e tabela Consult

---
+++ app/Models/Consult.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $location
+ * @property string $timezone
+ * @property Carbon $startTime
+ * @property Carbon $endTime
+ * @property array $today
+ * @property array $next_days
+ * @property Carbon $created_at
+ */
+class Consult extends Model
+{
+    protected $fillable = [
+        'location',
+        'timezone',
+        'startTime',
+        'endTime',
+        'today',
+        'next_days',
+        'created_at',
+    ];
+
+    protected $casts = [
+        'today'      => 'array',
+        'next_days'  => 'array',
+        'startTime'  => 'datetime',
+        'endTime'    => 'datetime',
+        'created_at' => 'datetime',
+    ];
+
+    public $timestamps = false;
+
+    protected function startTime(): Attribute
+    {
+        return Attribute::set(
+            fn ($value) => $value->setMinute(0)->setSecond(0)
+        );
+    }
+}
---
+++ database/migrations/2022_02_04_022623_create_consults_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConsultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('consults', function (Blueprint $table) {
+            $table->id();
+            $table->string('location');
+            $table->string('timezone');
+            $table->dateTime('startTime');
+            $table->dateTime('endTime');
+            $table->json('today');
+            $table->json('next_days');
+            $table->dateTime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('consults');
+    }
+}
