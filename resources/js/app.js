893e3da4ace785b6e06a71791cfc0c53305fc7a3
Felipe Alves <felipealves1550@gmail.com>
2022-03-11 00:27:08 -0300

    Adicionando o plugin de dragscroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,17 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
+import { dragscrollNext } from "vue-dragscroll";
 
 InertiaProgress.init()
 
 createInertiaApp({
     resolve: name => require(`./Pages/${name}`),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .mount(el)
+        const app = createApp({ render: () => h(App, props) })
+
+        app.directive('dragscroll', dragscrollNext);
+        app.use(plugin)
+        app.mount(el)
     },
 })
