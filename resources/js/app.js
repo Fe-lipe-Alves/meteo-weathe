e27003b20f460953d58c84c1fe730505e585dee9
Felipe Alves <felipealves1550@gmail.com>
2022-03-16 01:14:14 -0300

    Adicionando componente cors e link

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,20 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { createInertiaApp, Link } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
-import { dragscrollNext } from "vue-dragscroll";
+import { dragscrollNext } from "vue-dragscroll"
+
+const cors = require('cors')
 
 InertiaProgress.init()
 
 createInertiaApp({
     resolve: name => require(`./Pages/${name}`),
     setup({ el, App, props, plugin }) {
-        const app = createApp({ render: () => h(App, props) })
-
-        app.directive('dragscroll', dragscrollNext);
-        app.use(plugin)
-        app.mount(el)
+        createApp({ render: () => h(App, props) })
+            .directive('dragscroll', dragscrollNext)
+            .use(plugin)
+            .use(cors)
+            .component('InertiaLink', Link)
+            .mount(el)
     },
 })
