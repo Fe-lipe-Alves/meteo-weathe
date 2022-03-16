36d852be0ec7abe4182f76207a2a1eb544643017
Felipe Alves <felipealves1550@gmail.com>
2022-03-16 01:10:51 -0300

    Tratando pesquisa de cidades ou localização atual

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
+use App\Services\Geoapify;
 use App\Services\IpInfo;
 use App\Services\Tomorrow;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Inertia\Inertia;
 use Inertia\Response;
 use PHPUnit\Exception;
@@ -21,30 +24,60 @@ class HomeController extends Controller
     public function index(Request $request): Response
     {
         try {
-            //        $ipClient = $request->ip();
-            $ipClient = '186.193.112.12';
-            $consultIp = IpInfo::consultIp($ipClient);
-            $latLon = $consultIp['lat'] .','. $consultIp['lon'];
 
-            $tomorrow = new Tomorrow($latLon, $consultIp['timezone']);
+            if ($request->has('lat', 'lon', 'city')) {
+                $response = Tomorrow::consultCities($request->city);
+                $data = Arr::get($response, 'data.cities.0');
+                $timezone = $data['timezone'];
+                $latLon = Arr::only($data, ['lat', 'lng']);
+                $city = $data['name'];
+            } else {
+//                $ipClient = $request->ip();
+                $ipClient = '186.193.112.12';
+                $consultIp = IpInfo::consultIp($ipClient);
+                $latLon = Arr::only($consultIp, ['lat', 'lon']);
+                $timezone = $consultIp['timezone'];
+                $city = $consultIp['city'];
+            }
+            $latLon = implode(',', $latLon);
+
+            $tomorrow = new Tomorrow($latLon, $timezone);
 
             $now = $tomorrow->now();
 
             $today = $tomorrow->today();
-            $today['city'] = $consultIp['city'];
+            $today['city'] = $city;
 
             $nextDays = $tomorrow->nextDays(7);
 
             $nextHours = $tomorrow->nextHours(24);
 
+            $image = Image::query()->where()->first();
+
+            $imageBackground = asset($image->path);
+
             return Inertia::render('Home', [
                 'dataNow'       => $now,
                 'dataToday'     => $today,
                 'dataNextDays'  => $nextDays,
-                'dataNextHours' => $nextHours
+                'dataNextHours' => $nextHours,
+                'imageBackground' => $imageBackground,
+                'links'         => [
+                    'search_complete' => route('search-complete')
+                ]
             ]);
         } catch (Exception) {
             return Inertia::render('Error');
         }
+    }
+
+    public function searchComplete(Request $request)
+    {
+        $response = Geoapify::consult($request->search);
+
+        return response()->json([
+            'success' => true,
+            'cities' => $response,
+        ]);
     }
 }
