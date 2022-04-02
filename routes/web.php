9179f9b9c133a2a150d48169cee07c00a0951f1f
Felipe Alves <felipealves1550@gmail.com>
2022-04-02 01:43:28 -0300

    Adicionando rota dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('cache.headers:public;max_age=86400;etag')->group(function () {
-    Route::get('/', [HomeController::class, 'index']);
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/dashboard', [HomeController::class, 'images']);
