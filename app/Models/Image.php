e480f81d19e8406a90e91a4adcbdd2e8eace2462
Felipe Alves <felipealves1550@gmail.com>
2022-06-10 21:51:37 -0300

    Criando modelo de imagem

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,38 +3,27 @@
 namespace App\Models;
 
 use App\Support\Enums\Period;
-use App\Support\Enums\Weather;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Image extends Model
 {
     protected $fillable = [
         'path',
         'period',
-        'weather',
-        'active',
     ];
 
     protected $casts = [
-        'active' => 'boolean',
         'period' => Period::class,
-        'weather' => Weather::class,
     ];
 
-    /**
-     * Obtém a imagem conforme o código recebido
-     *
-     * @param int|string $weatherCode
-     * @return \Illuminate\Database\Eloquent\Builder|Model|object|null
-     */
-    public static function getByWeather(int|string $weatherCode)
+    public function weathers()
     {
-        return self::query()
-            ->where([
-                'weather' => Weather::get($weatherCode),
-                'active' => true,
-            ])
-            ->inRandomOrder()
-            ->first();
+        return $this->belongsToMany(Weather::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return Storage::url($this->attributes['path']);
     }
 }
